ca75a5467b3e651238721bc9c2d8c79ee2711fc9
ez4lionky <codex.lxy@gmail.com>
2022-05-17 10:33:44 +0800

    Update 05-17.

--- 05-17/953.py
+++ 05-17/953.py
@@ -1,21 +1,29 @@
+from itertools import pairwise
+
+
+# class Solution:
+#     def isAlienSorted(self, words: List[str], order: str) -> bool:
+#         def if_sorted(w0, w1, order):
+#             l = min(len(w0), len(w1))
+#             for i in range(l):
+#                 s0, s1 = order.find(w0[i]), order.find(w1[i])
+#                 if s0 > s1:
+#                     return False
+#                 elif s0 < s1:
+#                     return True
+#             if len(w0) > l:
+#                 return False
+#             return True
+#
+#         for i in range(len(words) - 1):
+#             w0, w1 = words[i], words[i+1]
+#             flag = if_sorted(w0, w1, order)
+#             if not flag:
+#                 return False
+#         return True
+
 class Solution:
     def isAlienSorted(self, words: List[str], order: str) -> bool:
-        def if_sorted(w0, w1, order):
-            l = min(len(w0), len(w1))
-            for i in range(l):
-                s0, s1 = order.find(w0[i]), order.find(w1[i])
-                if s0 > s1:
-                    return False
-                elif s0 < s1:
-                    return True
-            if len(w0) > l:
-                return False
-            return True
-
-        for i in range(len(words) - 1):
-            w0, w1 = words[i], words[i+1]
-            flag = if_sorted(w0, w1, order)
-            if not flag:
-                return False
-        return True
+        index = {c: i for i, c in enumerate(order)}
+        return all([s <= t for s, t in pairwise([index[c] for c in word] for word in words)])
 
