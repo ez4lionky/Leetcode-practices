a09a18b3761456eedeadbd4027bc313f25c89c9e
ez4lionky <codex.lxy@gmail.com>
2022-05-21 21:11:37 +0800

    Update new methods for 961.py

--- 05-21/961.py
+++ 05-21/961.py
@@ -1,9 +1,34 @@
+import random
+
+
+# class Solution:
+#     def repeatedNTimes(self, nums: List[int]) -> int:
+#         s = set()
+#         for n in nums:
+#             if n in s:
+#                 return n
+#             s.add(n)
+#         return -1
+
+
+# class Solution:
+#     def repeatedNTimes(self, nums: List[int]) -> int:
+#         n = len(nums)
+#         for gap in range(1, 4):
+#             for i in range(n - gap):
+#                 if nums[i] == nums[i + gap]:
+#                     return nums[i]
+        
+#         # 不可能的情况
+#         return -1
+
+
 class Solution:
     def repeatedNTimes(self, nums: List[int]) -> int:
-        s = set()
-        for n in nums:
-            if n in s:
-                return n
-            s.add(n)
-        return -1
+        n = len(nums)
+
+        while True:
+            x, y = random.randrange(n), random.randrange(n)
+            if x != y and nums[x] == nums[y]:
+                return nums[x]
 
