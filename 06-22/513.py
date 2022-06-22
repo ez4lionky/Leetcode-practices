e623bc5491dce1eeece155fe72134db5aab90cbe
ez4lionky <codex.lxy@gmail.com>
2022-06-22 10:13:17 +0800

    Updated level traversal version of 06-22 513.py

--- 06-22/513.py
+++ 06-22/513.py
@@ -1,3 +1,6 @@
+from collections import deque
+
+
 # Definition for a binary tree node.
 # class TreeNode:
 #     def __init__(self, val=0, left=None, right=None):
@@ -21,3 +24,17 @@ class Solution:
         dfs(root, 1)
         return self.val
 
+
+class Solution:
+    val, max_h = 0, 0
+    def findBottomLeftValue(self, root: Optional[TreeNode]) -> int:
+        q = deque([[root, 1]])
+        while q:
+            for _ in range(len(q)):
+                r, h = q.popleft()
+                if h > self.max_h:
+                    self.max_h = h
+                    self.val = r.val
+                if r.left is not None: q.append([r.left, h+1])
+                if r.right is not None: q.append([r.right, h+1])
+        return self.val
