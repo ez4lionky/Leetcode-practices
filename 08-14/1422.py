205cc99309d15027caac217fb6ea5a727de83b45
lionky <codex.lxy@gmail.com>
2022-08-14 12:04:59 +0800

    Finished 08-14 and added official solution.

--- 08-14/1422.py
+++ 08-14/1422.py
@@ -1,17 +1,25 @@
+# class Solution:
+#     def maxScore(self, s: str) -> int:
+#         st, l = 1, len(s)
+#         zero_num, one_num = s[:st] == '0', s[st:].count('1')
+#         res = zero_num + one_num
+#         # print(zero_num, one_num, res)
+#         while st < l-1:
+#             zero_num += s[st] == '0'
+#             one_num -= s[st] == '1'
+#             if zero_num + one_num > res:
+#                 res = zero_num + one_num
+#             st += 1
+#         return res
+
+
 class Solution:
     def maxScore(self, s: str) -> int:
-        st, l = 1, len(s)
-        zero_num, one_num = s[:st] == '0', s[st:].count('1')
-        res = zero_num + one_num
-        # print(zero_num, one_num, res)
-        while st < l-1:
-            zero_num += s[st] == '0'
-            one_num -= s[st] == '1'
-            if zero_num + one_num > res:
-                res = zero_num + one_num
-            st += 1
-        return res
-
+        ans = score = (s[0] == '0') + s[1:].count('1')
+        for c in s[1:-1]:
+            score += 1 if c == '0' else -1
+            ans = max(ans, score)
+        return ans
 
 
 if __name__ == "__main__":
