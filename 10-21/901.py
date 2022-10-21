f61604bd005fdd091da74433f618b6125245d187
lionky <codex.lxy@gmail.com>
2022-10-21 14:00:10 +0800

    Update a more unified method for 10-21/901.py

--- 10-21/901.py
+++ 10-21/901.py
@@ -2,21 +2,31 @@ from collections import deque
 
 
 class StockSpanner:
-
     def __init__(self):
-        self.prices = deque()
-        self.index = -1
+        self.stack = [(-1, inf)]
+        self.idx = -1
 
     def next(self, price: int) -> int:
-        self.index += 1
-        while self.prices and price >= self.prices[-1][0]:
-            _ = self.prices.pop()
-        if len(self.prices) == 0:
-            res = self.index + 1
-        else:
-            res = self.index - self.prices[-1][1]
-        self.prices.append([price, self.index])
-        return res
+        self.idx += 1
+        while price >= self.stack[-1][1]:
+            self.stack.pop()
+        self.stack.append((self.idx, price))
+        return self.idx - self.stack[-2][0]
+
+    # def __init__(self):
+    #     self.prices = deque()
+    #     self.index = -1
+    #
+    # def next(self, price: int) -> int:
+    #     self.index += 1
+    #     while self.prices and price >= self.prices[-1][0]:
+    #         _ = self.prices.pop()
+    #     if len(self.prices) == 0:
+    #         res = self.index + 1
+    #     else:
+    #         res = self.index - self.prices[-1][1]
+    #     self.prices.append([price, self.index])
+    #     return res
 
 # Your StockSpanner object will be instantiated and called as such:
 # obj = StockSpanner()
