8e4d03fda8c955f6cddc2bb7634a84638448203c
lionky <codex.lxy@gmail.com>
2022-12-23 11:03:06 +0800

    Update 2011.py

--- 12-23/2011.py
+++ 12-23/2011.py
@@ -1,8 +1,13 @@
+# class Solution:
+#     def finalValueAfterOperations(self, operations: List[str]) -> int:
+#         res = 0
+#         for op in operations:
+#             x = 1 if '+' in op else -1
+#             res += x
+#         return res
+
+
 class Solution:
     def finalValueAfterOperations(self, operations: List[str]) -> int:
-        res = 0
-        for op in operations:
-            x = 1 if '+' in op else -1
-            res += x
-        return res
+        return sum(44 - ord(op[1]) for op in operations)
 
