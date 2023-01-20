e18ba98a3edffe6fec3f09efb219294789e513de
lionky <codex.lxy@gmail.com>
2023-01-20 20:49:11 +0800

    Finished 01-20.

--- 01-20/1817.py
+++ 01-20/1817.py
@@ -1,15 +1,26 @@
 from collections import defaultdict
 
 
+# class Solution:
+#     def findingUsersActiveMinutes(self, logs: List[List[int]], k: int) -> List[int]:
+#         counter = defaultdict(set)
+#         for uid, mid in logs:
+#             counter[uid].add(mid)
+#         res = [0 for _ in range(k)]
+#         for k, v in counter.items():
+#             if 0 < len(v):
+#                 res[len(v)-1] += 1
+#         return res
+
+
 class Solution:
     def findingUsersActiveMinutes(self, logs: List[List[int]], k: int) -> List[int]:
         counter = defaultdict(set)
         for uid, mid in logs:
             counter[uid].add(mid)
-        res = [0 for _ in range(k)]
-        for k, v in counter.items():
-            if 0 < len(v):
-                res[len(v)-1] += 1
+        res = [0] * k
+        for uid, v in counter.items():
+            res[len(v)-1] += 1
         return res
 
 
